5980a2ac41c023435df3acb207644f5d20a2f61f
JTEnglish <je787413@ohio.edu>
2018-01-26 11:28:01 -0600

    added nth prime computation to server and byte string handling

--- client.py
+++ client.py
@@ -1,18 +1,27 @@
 import time
 from socket import *
 
-for ping in range(10):
-    clientSocket = socket(AF_INET, SOCK_DGRAM)
-    clientSocket.settimeout(1)
-    message = str.encode(str(ping))
-    addr = ("127.0.0.1", 12000)
+def main():
+    print("Msg\tTime\tResult")
+    for ping in range(1, 100):
+        clientSocket = socket(AF_INET, SOCK_DGRAM)
+        clientSocket.settimeout(1)
+        message = str.encode(str(ping))
+        addr = ("127.0.0.1", 12000)
 
-    start = time.time()
-    clientSocket.sendto(message, addr)
+        start = time.time()
+        clientSocket.sendto(message, addr)
+        try:
+            data, server = clientSocket.recvfrom(1024)
+            data = str(data)[2:-1]
+            end = time.time()
+            elapsed = end - start
+            print('%d\t%d\t%s' % (ping, elapsed, data))
+        except timeout:
+            print('REQUEST TIMED OUT')
+
+if __name__ == "__main__":
     try:
-        data, server = clientSocket.recvfrom(1024)
-        end = time.time()
-        elapsed = end - start
-        print('%s %d %d' % (data, ping, elapsed))
-    except timeout:
-        print('REQUEST TIMED OUT')
+        main()
+    except KeyboardInterrupt:
+        print("\nServer Off")
--- server.py
+++ server.py
@@ -1,13 +1,41 @@
-import random
+import math
 from socket import *
 
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', 12000))
+def is_prime(val):
+    for i in range(2, int(math.sqrt(val) + 1)):
+        if (val % i) == 0:
+            return False
+    return True
 
-connections = 0
-while True:
-    rand = random.randint(0, 10)
-    message, address = serverSocket.recvfrom(1024)
-    connections += 1
-    response = 'msg' + str(connections) + ': ' + str(message)
-    serverSocket.sendto(str.encode(response), address)
+def nth_prime(n):
+    i = 0
+    val = 2
+    while i < n:
+        if is_prime(val):
+            i += 1
+            if i == n:
+                return val
+        val += 1
+
+def main():
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind(('', 12000))
+
+    print("Server On")
+    while True:
+        message, address = serverSocket.recvfrom(1024)
+
+        print("Message from", address, ":", message)
+
+        # n =  re.sub("[^0-9]", "", str(message))
+        n = str(message)[2:-1]
+        prime = nth_prime(int(n))
+
+        response = str(prime)
+        serverSocket.sendto(str.encode(response), address)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nServer Off")
